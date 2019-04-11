29a53f1089ddc5f79aa0fde7e155d60a954c3374
Mason Thomas <Kandleseatwicks@gmail.com>
2019-04-11 12:15:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ var app = express();
 var server = app.listen(process.env.PORT || 3000);
 var usersCurrentMouseData = [];
 
+var handshake;
+
 app.use(express.static('public'));
 
 console.log("My socket server is running");
@@ -15,7 +17,9 @@ var io = socket(server);
 io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
+  let handshake = socket.handshake;
   console.log('new connection: ' + socket.id);
+  console.log(handshake);
 
   socket.on('mouse', mouseMsg);
   for (var data of usersCurrentMouseData) {
