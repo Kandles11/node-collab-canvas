9bbc900304ece0216cb3b7eef83cfad47569ed72
Mason Thomas <mason@Masons-Mac-mini.local>
2019-04-27 20:49:30 -0500

    FIX

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ var socket = require('socket.io');
 
 var io = socket(server);
 
-
+io.sockets.on('connection', newConnection);
 
 function newConnection(socket) {
   //let handshake = socket.handshake;
   console.log('new connection: ' + socket.id);
   //console.log(handshake);
-
-
 
   socket.on('mouse', mouseMsg);
   for (var data of usersCurrentMouseData) {
